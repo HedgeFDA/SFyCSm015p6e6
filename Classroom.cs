3438e58bc5415635d77e6d50d53c4929febe0994
Ilya Boiko <boiko.ilya.evg@gmail.com>
2025-03-07 17:06:30 +0500

    15.6.6

---
+++ Classroom.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SFyCSm015p6e6;
+
+/// <summary>
+/// Класс реализующий классную комнату
+/// </summary>
+public class Classroom
+{
+    /// <summary>
+    /// Ученики
+    /// </summary>
+    public List<string> Students = new List<string>();
+
+    /// <summary>
+    /// Инициализирует новый экземпляр класса.
+    /// </summary>
+    public Classroom()
+    {
+        // Конструктор по умолчанию
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SFyCSm015p6e6;
+
+class Program
+{
+    /// <summary>
+    /// Функция собирающая учеников всех классов в один список
+    /// </summary>
+    /// <param name="classes">Массив классных комнат</param>
+    /// <returns>Массив всех учеников</returns>
+    static string[] GetAllStudents(Classroom[] classes)
+    {
+        return classes.SelectMany(c => c.Students).ToArray();
+    }
+
+    /// <summary>
+    /// Процедура реализующая основной алгоритм работы приложения.
+    /// </summary>
+    static void PerformMain()
+    {
+        var classes = new[]
+        {
+               new Classroom { Students = {"Evgeniy", "Sergey", "Andrew"}, },
+               new Classroom { Students = {"Anna", "Viktor", "Vladimir"}, },
+               new Classroom { Students = {"Bulat", "Alex", "Galina"}, }
+           };
+        
+        var allStudents = GetAllStudents(classes);
+
+        Console.WriteLine(string.Join(" ", allStudents));
+    }
+
+    /// <summary>
+    /// Главная точка входа приложения
+    /// </summary>
+    /// <param name="args">Аргументы командной строки при запуске приложения.</param>
+    static void Main(string[] args)
+    {
+        PerformMain();
+    }
+}
